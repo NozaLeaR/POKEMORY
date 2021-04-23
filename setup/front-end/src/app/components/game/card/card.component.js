ebebd7b2e766b7c79d529c45af5447790e487250
NozaLeaR <olivier.marcenac@epfedu.fr>
2021-04-23 15:54:50 +0200

    7.1

--- setup/front-end/src/app/components/game/card/card.component.js
+++ setup/front-end/src/app/components/game/card/card.component.js
@@ -1,59 +1,54 @@
-// TODO Step 7 import "./card.component.html"
+import './card.component.css';
+import template from './card.component.html';
+import { Component } from '../../../utils/component';
 
-// TODO Step 7 remove this closure
 
-    class CardComponent {   
-        constructor(id) {
-             // is this card flipped ?
+export class CardComponent extends Component {
+    constructor(id) {
+        super('card')
+        // is this card flipped ?
         this._flipped = false;
-
         // has the matching card has been discovered already ?
         this.matched = false;
-
         this._id = id;
 
         // TODO Step 7: We can access the element from the component class that built an element from the template
         // TODO Step 7: don't use document.getElementById anymore
-        this._elt = document.getElementById('card-template').content.cloneNode(true).firstElementChild;
-        this._imageElt = this._elt.querySelector('.card-wrapper');
-        // TODO Step 1: Change images location to ./card/assets/***.png
-        // TODO Step 3.2: use template literals
-        // TODO Step 7: Update the path for images
-        this._imageElt.querySelector('img.front-face').src = './card/assets/card-' + this._id + '.png';
-        this._imageElt.querySelector('img.back-face').src = './card/assets/back.png';
-        }
 
-         getElement() {
-            return this._elt;
-        }
-    
-         flip() {
-            this._imageElt.classList.toggle('flip');
-            this._flipped = !this._flipped;
-        }
-    
-         equals(card) {
-            return card._id === this._id;
-        }
+        const elt = super.getElement();
+        this._imageElt = elt.querySelector('.card-wrapper');
+        this._imageElt.querySelector('img.front-face').src = `src/app/components/game/card/assets/card-${this._id}.png`;
+        this._imageElt.querySelector('img.back-face').src = 'src/app/components/game/card/assets/back.png';
 
-        get flipped() {
-            return this._flipped;
-        }
 
-        get host () {
-            debugger
-            throw new Error(atob('VG9vIGJhZCEgV2l0aG91dCBjbG9zdXJlLCBJIGNhbiBjb2xsaWRlIGJ5IG1pc3Rha2VzICB3aXRoIGdsb2JhbCB2YXJpYWJsZXMgdGhhdCBoYXZlIGJlZW4gc2V0IGluIGFub3RoZXIgZmlsZQ'));
-        }
     }
 
-    // TODO Step 6 implement getTemplate() {}
+    getTemplate() {
+        return template;
+    }
 
-    var environment = {
-        api: {}
-    };
+    flip() {
+        this._imageElt.classList.toggle('flip');
+        this._flipped = !this._flipped;
+    }
 
+    equals(card) {
+        return card._id === this._id;
+    }
 
-    // put component in global scope, tu be runnable right from the HTML.
-    // TODO Step 7 export CardComponent
-    window.CardComponent = CardComponent;
+    get flipped() {
+        return this._flipped;
+    }
+
+    get host() {
+        debugger
+        throw new Error(atob('VG9vIGJhZCEgV2l0aG91dCBjbG9zdXJlLCBJIGNhbiBjb2xsaWRlIGJ5IG1pc3Rha2VzICB3aXRoIGdsb2JhbCB2YXJpYWJsZXMgdGhhdCBoYXZlIGJlZW4gc2V0IGluIGFub3RoZXIgZmlsZQ'));
+    }
+}
+
+// TODO Step 6 implement getTemplate() {}
+
+var environment = {
+    api: {}
+};
 
--- setup/front-end/src/app/components/game/game.component.js
+++ setup/front-end/src/app/components/game/game.component.js
@@ -1,135 +1,135 @@
 // TODO Step 7 import "./game.component.html"
+import { Component } from '../../utils/component';
 import { parseUrl } from '../../utils/utils';
+import template from "./game.component.html"
+import {CardComponent} from "./card/card.component.js"
+import "./game.component.html"
 
-   // TODO Step 7 remove this closure
-    let environment = {
-        api: {
-            host: 'http://localhost:8081'
-        }
-    };
+// TODO Step 7 remove this closure
+let environment = {
+    api: {
+        host: 'http://localhost:8081'
+    }
+};
 
-    // TODO Step 3.1 create a class
-    /* class GameComponent constructor */
-    class GameComponent {
-        constructor(id) {
-            // gather parameters from URL
-            let params = parseUrl();
-            // save player name & game ize
-            this._name = params.name;
-            this._size = parseInt(params.size) || 9;
-            this._flippedCard = null;
-            this._matchedPairs = 0;
-        }
+// TODO Step 3.1 create a class
+/* class GameComponent constructor */
+export class GameComponent extends Component {
+    constructor(id) {
+        super('game');
+        // gather parameters from URL
+        let params =
+         parseUrl();
+        // save player name & game ize
+        this._name = params.name;
+        this._size = parseInt(params.size) || 9;
+        this._flippedCard = null;
+        this._matchedPairs = 0;
+    }
 
-        async init() {
-            // fetch the cards configuration from the server
-            const config = await this.fetchConfig();
-            // TODO Step 3.2: use arrow function
-            // create a card out of the config
-            // TODO Step 3.3: use Array.map()
-            this._cards = config.ids.map(x => new CardComponent(x));
+    getTemplate() {
+        return template;
+    }
 
-            this._boardElement = document.querySelector('.cards');
+    async init() {
+        // fetch the cards configuration from the server
+        const config = await this.fetchConfig();
+        // TODO Step 3.2: use arrow function
+        // create a card out of the config
+        // TODO Step 3.3: use Array.map()
+        this._cards = config.ids.map(x => new CardComponent(x));
 
-            this._cards.forEach( // TODO Step 3.3: use Array.forEach()
-                ((x) => {
-                    let card = x;
-                    this._boardElement.appendChild(card.getElement());
-                    card.getElement().addEventListener('click', () => { this._flipCard(card) }); // TODO use arrow function.
-                }));
-            this.start();
+        this._boardElement = document.querySelector('.cards');
 
-        }
+        this._cards.forEach( // TODO Step 3.3: use Array.forEach()
+            ((x) => {
+                let card = x;
+                this._boardElement.appendChild(card.getElement());
+                card.getElement().addEventListener('click', () => { this._flipCard(card) }); // TODO use arrow function.
+            }));
+        this.start();
 
-        start() {
-            this._startTime = Date.now();
-            let seconds = 0;
+    }
+
+    start() {
+        this._startTime = Date.now();
+        let seconds = 0;
+        // TODO Step 3.2: use template literals
+        document.querySelector('nav .navbar-title').textContent = `Player: ${this._name}. Elapsed time:${seconds++}`;
+
+        this._timer = setInterval(() => { // TODO Step 3.2: use arrow function
             // TODO Step 3.2: use template literals
             document.querySelector('nav .navbar-title').textContent = `Player: ${this._name}. Elapsed time:${seconds++}`;
+        }, 1000);
+    }
 
-            this._timer = setInterval(() => { // TODO Step 3.2: use arrow function
-                // TODO Step 3.2: use template literals
-                document.querySelector('nav .navbar-title').textContent = `Player: ${this._name}. Elapsed time:${seconds++}`;
-            }, 1000);
+    gotoScore() {
+        let timeElapsedInSeconds = Math.floor((Date.now() - this._startTime) / 1000);
+        clearInterval(this._timer);
+
+        setTimeout(() => { window.location = `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`; }, 750);
+        // TODO Step 7: change path to: `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`;
+    }
+
+    async fetchConfig() {
+        return fetch(`${environment.api.host}/board?size=${this._size}`, {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .catch(error => console.log("Fetch config error", error));
+    }
+
+
+    _flipCard(card) {
+        if (this._busy) {
+            return;
         }
 
-        gotoScore() {
-            let timeElapsedInSeconds = Math.floor((Date.now() - this._startTime) / 1000);
-            clearInterval(this._timer);
-
-            setTimeout(() => { window.location = `../score/score.component.html?name=${this._name}&size=${this._size}&time=${timeElapsedInSeconds}`; }, 750);
-            // TODO Step 3.2: use arrow function.
-            // TODO Step 1: replace with '../score.component.html' location
-            // TODO Step 3.2: use template literals
-            // TODO Step 7: change path to: `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`;
-            // TODO Step 3.2: Why bind(this)?
+        if (card.flipped) {
+            return;
         }
 
-        async fetchConfig() {
-            return fetch(`${environment.api.host}/board?size=${this._size}`, {
-                method: "GET"
-            })
-                .then(response => response.json())
-                .catch(error => console.log("Fetch config error", error));
-        }
+        // flip the card
+        card.flip();
 
+        // if flipped first card of the pair
+        if (!this._flippedCard) {
+            // keep this card flipped, and wait for the second card of the pair
+            this._flippedCard = card;
+        } else {
+            // second card of the pair flipped...
 
-        _flipCard(card) {
-            if (this._busy) {
-                return;
-            }
+            // if cards are the same
+            if (card.equals(this._flippedCard)) {
+                this._flippedCard.matched = true;
+                card.matched = true;
+                this._matchedPairs += 1;
 
-            if (card.flipped) {
-                return;
-            }
+                // reset flipped card for the next turn.
+                this._flippedCard = null;
 
-            // flip the card
-            card.flip();
-
-            // if flipped first card of the pair
-            if (!this._flippedCard) {
-                // keep this card flipped, and wait for the second card of the pair
-                this._flippedCard = card;
+                if (this._matchedPairs === this._size) {
+                    this.gotoScore();
+                }
             } else {
-                // second card of the pair flipped...
+                this._busy = true;
 
-                // if cards are the same
-                if (card.equals(this._flippedCard)) {
-                    this._flippedCard.matched = true;
-                    card.matched = true;
-                    this._matchedPairs += 1;
+                // cards did not match
+                // wait a short amount of time before hiding both cards
+                // TODO Step 3.2 use arrow function
+                setTimeout(() => {
+                    // hide the cards
+                    this._flippedCard.flip();
+                    card.flip();
+                    this._busy = false;
 
                     // reset flipped card for the next turn.
                     this._flippedCard = null;
-
-                    if (this._matchedPairs === this._size) {
-                        this.gotoScore();
-                    }
-                } else {
-                    this._busy = true;
-
-                    // cards did not match
-                    // wait a short amount of time before hiding both cards
-                    // TODO Step 3.2 use arrow function
-                    setTimeout(() => {
-                        // hide the cards
-                        this._flippedCard.flip();
-                        card.flip();
-                        this._busy = false;
-
-                        // reset flipped card for the next turn.
-                        this._flippedCard = null;
-                    }, 500);
-                }
+                }, 500);
             }
         }
     }
+}
 
-    // TODO Step 6 implement getTemplate() {}
-
-
-    // put component in global scope, tu be runnable right from the HTML.
-    // TODO Step 7: export GameComponent
-    window.GameComponent = GameComponent;
 
 
